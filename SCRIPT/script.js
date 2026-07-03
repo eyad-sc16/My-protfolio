56f124e40747a181a421178c1f9f31019960b942
Eyad nasser <eyadnasser499@gmail.com>
2026-07-03 05:33:20 +0300

    finishig the mobile responsive

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -3,11 +3,15 @@
 const advantagesContent = document.querySelectorAll(".advantages-content");
 const innerContent = document.querySelectorAll(".inner-advantges-content");
 const contentArrowBox = document.querySelectorAll(".arrow-container");
+const contentTitle = document.querySelectorAll(".title-text");
+const hoverWord = document.querySelectorAll(".hover-word-container ");
 
 for (let i = 0; i < advantagesContent.length; i++) {
   advantagesContent[i].addEventListener("click", function () {
-    innerContent[i].classList.toggle("hidden");
+    innerContent[i].classList.toggle("inner-open");
     contentArrowBox[i].classList.toggle("rotate");
+    contentTitle[i].classList.toggle("hidden");
+    hoverWord[i].classList.toggle("hidden");
   });
 }
 
